002e04f38ae2b93161f29bd1ee1c80fa9e2a7f37
root <root@vps565428.ovh.net>
2018-10-19 17:05:12 +0200

    initial

---
+++ application/translations/de/lianamailer_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *    liana-mailer
+ *  Copyright (C) 2018  bestlife AG
+ *  info:  oxid@bestlife.ag
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+$aLang = [
+	'charset'                         => 'UTF-8',
+	'LIANA_NEWSLETTER_NOT_CONFIGURED' => 'Newsletter Schnittstelle ist nicht ausreichend konfiguriert',
+	'LIANA_NEWSLETTER_I_ACCEPT_GDPR' => 'Ich habe die Bestimmungen für Datenschutz und -verarbeitung gelesen und akzeptiert',
+	'LIANA_NEWSLETTER_PLEASE_ACCEPT_GDPR' => 'Für die Anmeldung zum Newsletter müssen Sie unsere Bestimmungen akzeptieren!',
+	'LIANA_NEWSLETTER_SOMETHING_FAILED' => 'Leider ist '
+
+
+];
